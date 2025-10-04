92f342193e578799af426a8702bcb77553fb0643
KanapkazChlebem <k.tarkowski@wp.pl>
2025-10-04 16:48:32 +0200

    Change to AddForceAtPoint

--- Assets/Scripts/ObjectMovement/CameraObjectMovementComponent.cs
+++ Assets/Scripts/ObjectMovement/CameraObjectMovementComponent.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using FMOD;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class CameraObjectMovementComponent : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class CameraObjectMovementComponent : MonoBehaviour
     [SerializeField] private float stoppingOffset = 0.1f;
     private bool _enable = false;
     private bool _isOn = false;
+    private Vector3 _forcePoint;
 
     private void Start()
     {
@@ -19,14 +21,30 @@ public class CameraObjectMovementComponent : MonoBehaviour
 
     private IEnumerator MoveObjectToCameraCentre()
     {
+        Vector3 targetPoint = transform.position + Vector3.forward * distanceFromCamera;
+        Ray ray = new Ray(transform.position, (targetPoint - transform.position).normalized);
+        RaycastHit hit;
+        Collider col = manipulatedObject.GetComponent<Collider>();
+        if (Physics.Raycast(ray, out hit, distanceFromCamera * 2f))
+        {
+            _forcePoint = hit.point;
+            Debug.Log(_forcePoint);
+        }
+        else
+        {
+            _forcePoint = Vector3.zero;
+            Debug.Log("Object not hit with Raycast");
+            yield break;
+        }
+
         while (_enable)
         {
-            Vector3 targetPoint = transform.position + Vector3.forward * distanceFromCamera;
-            Vector3 dir = (targetPoint - manipulatedObject.position).normalized;
+            _forcePoint = col.ClosestPoint(targetPoint);
+            targetPoint = transform.position + Vector3.forward * distanceFromCamera;
+            Vector3 dir = (targetPoint - _forcePoint).normalized;
             if (distanceFromCamera > 0.1f)
             {
-                //TODO: add force at point
-                manipulatedObject.AddForce(dir * forceMultiplier);
+                manipulatedObject.AddForceAtPosition(dir * forceMultiplier, _forcePoint);
             }
 
             if (manipulatedObject.linearVelocity.magnitude > maxSpeed)
