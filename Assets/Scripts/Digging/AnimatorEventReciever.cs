7b5f60066123aa4139d69196aeb294b98d835ec6
KanapkazChlebem <k.tarkowski@wp.pl>
2025-10-05 03:05:56 +0200

    Change digging cooldown to animation end

--- Assets/Scripts/Digging/AnimatorEventReciever.cs
+++ Assets/Scripts/Digging/AnimatorEventReciever.cs
@@ -4,15 +4,21 @@ public class AnimatorEventReciever : MonoBehaviour
 {
 
     private PlayerController _playerController;
+
+    private PlayerDiggingComponent _playerDiggingComponent;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _playerController = GetComponentInParent<PlayerController>();
         if(!_playerController) Debug.LogError($"Playercontroller not found for {this}");
+        _playerDiggingComponent = GetComponentInParent<PlayerDiggingComponent>();
+        if (!_playerController) Debug.LogError($"Player digging component not found for {this}");
+
     }
 
     public void DigAnimationEnded()
     {
         _playerController.DiggingAnimationEnded();
+        _playerDiggingComponent.AnimationEnded();
     }
 }
